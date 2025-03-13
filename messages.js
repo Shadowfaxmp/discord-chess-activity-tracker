6b6aa9336e182f365b2600d883388a4c7f437b01
Shadowfaxmp <shadowfaxmp@gmail.com>
2025-03-12 20:22:26 -0400

    Added functions to get the number of move of the game of the opening name

--- messages.js
+++ messages.js
@@ -1,4 +1,6 @@
-export function getRandomWinMsg(timeControl, username, ratingChange, newRating, gameUrl, loss_type) {
+import {getGameLengthFromPGN, getOpeningName} from "./chess_utils.js";
+
+export function getRandomWinMsg(timeControl, username, ratingChange, newRating, game, loss_type) {
     console.log(loss_type)
     let randomWinMessages;
     if(loss_type === "abandoned") {
@@ -13,7 +15,6 @@ export function getRandomWinMsg(timeControl, username, ratingChange, newRating, 
             `Dang! **${username}** beat their opponent so hard they rage quit! **${username}** gained ${ratingChange} points and now has a ${newRating} rating`,
             `What's this? A rage quit? **${username}** just made their opponent resign with their amazing accuracy. They now have a ${newRating} rating after gaining ${ratingChange} points!`,
             `**${username}** was beating their opponent so hard they decided to resign. New rating is ${newRating} after gaining ${ratingChange} points!`,
-            `GothamChess is disappointed, **${username}** just resigned their ${timeControl} game losing ${ratingChange} points. New Rating: ${newRating} after gaining ${ratingChange}`,
         ]
     } else {
         randomWinMessages = [
@@ -22,19 +23,19 @@ export function getRandomWinMsg(timeControl, username, ratingChange, newRating, 
             `**${username}** just bullied an orphan for ${ratingChange} points of ${timeControl} rating. New ${timeControl} rating: ${newRating}. Was it worth it?`,
             `**${username}** isn't beating the cheating allegations. ${username.charAt(0).toUpperCase() + username.slice(1).toLowerCase()} just got +${ratingChange} ${timeControl} rating`,
             `Democracy? Who needs democracy when **${username}**’s king remains uncaptured! ${ratingChange} gloriously won from ${timeControl} will be bestowed to this new kingdom.`,
-            `The smell of victory has been bestowed upon **${username}**, who has just received ${ratingChange} in ${timeControl} rating. New rating: ${newRating}.`,
+            `The smell of victory has been bestowed upon **${username}**, who has just received ${ratingChange} in ${timeControl} rating.`,
             `What’s this I see 😳? **${username}** just got ${ratingChange} in a ${timeControl} game? New rating is ${newRating}, wonderful!`,
             `Winner winner, **${username}** received ${ratingChange} in a ${timeControl} game! Now have some chicken.`,
-            `Get out the fireworks; **${username}** got a ${ratingChange} in a ${timeControl} game!`,
+            `Get out the fireworks; **${username}** got a ${ratingChange} points in a ${timeControl} game!`,
             `**${username}** pleased the Hokie Bird with a win from a ${timeControl} game earning ${ratingChange} points. UVA be seething.`,
             `Off with their head 🔪! **${username}** just sent another king to the guillotine, gaining ${ratingChange} points in ${timeControl}. New rating: ${newRating}. Vive la révolution!`
         ];
     }
 
-    return format_message(randomWinMessages[Math.floor(Math.random() * randomWinMessages.length)], gameUrl, username)
+    return format_message(randomWinMessages[Math.floor(Math.random() * randomWinMessages.length)], game, username, 0x630031, newRating)
 }
 
-export function getRandomLoseMsg(timeControl, username, ratingChange, newRating, gameUrl, loss_type) {
+export function getRandomLoseMsg(timeControl, username, ratingChange, newRating, game, loss_type) {
     console.log(loss_type)
     let randomLoseMessages;
     if(loss_type === "abandoned") {
@@ -44,6 +45,7 @@ export function getRandomLoseMsg(timeControl, username, ratingChange, newRating,
             `Run outta bandwidth? **${username}** just disconnected losing ${Math.abs(ratingChange)} ${timeControl} points. New Rating: ${newRating}`,
             `**${username}** is taking Gothom's advice of never resigning - by just disconnecting their games instead... **${username}** just lost ${Math.abs(ratingChange)} ${timeControl} points. New Rating: ${newRating}`
         ];
+
     } else {
         randomLoseMessages = [
             `**${username}** just lost to a small child 🫢. Lost ${Math.abs(ratingChange)} points in ${timeControl} rating. New ${timeControl} rating: ${newRating}`,
@@ -60,55 +62,72 @@ export function getRandomLoseMsg(timeControl, username, ratingChange, newRating,
         ];
     }
 
-    return format_message(randomLoseMessages[Math.floor(Math.random() * randomLoseMessages.length)], gameUrl, username, 0xCF4420);
+    return format_message(randomLoseMessages[Math.floor(Math.random() * randomLoseMessages.length)], game, username, 0xCF4420, newRating);
 }
 
-export function getRandomDrawMsg(timeControl, username, ratingChange, newRating, gameUrl) {
+export function getRandomDrawMsg(timeControl, username, ratingChange, newRating, game) {
     const randomDrawMessages = [
-        `A tale as old as time: **${username}** couldn't close the game. A draw in ${timeControl}. Rating change: ${ratingChange}. New rating: ${newRating}.`,
-        `Two titans clashed... and neither won. **${username}** drew a ${timeControl} game. Rating change: ${ratingChange}. New rating: ${newRating}.`,
-        `Not a win, not a loss, just a whole lot of wasted time. **${username}** drew in ${timeControl}. New rating: ${newRating}.`,
-        `Some say a draw is just a loss without consequences. **${username}** couldn't decide today. Rating change: ${ratingChange}. New rating: ${newRating}.`,
-        `50 moves, 3 blunders, 2 missed mates, and a stalemate. **${username}** secures the most unsatisfying result in chess: a draw. Rating change: ${ratingChange}. New rating: ${newRating}.`,
-        `It's not about winning or losing, it's about sending a message. **${username}** just drew a ${timeControl} game. New rating: ${newRating}.`,
-        `You either die a hero or live long enough to see yourself draw. **${username}** just got ${ratingChange} points with a ${timeControl} draw. New rating: ${newRating}.`,
-        `Another draw? **${username}** might be allergic to winning. ${timeControl} game ends in a tie. New rating: ${newRating}.`,
-        `Somewhere, a chess coach just sighed. **${username}** drew in ${timeControl}. New rating: ${newRating}.`,
-        `A draw? Really? **${username}** out here making sure nobody has fun. New rating: ${newRating}.`,
+        `A tale as old as time: **${username}** couldn't close the game. A draw in ${timeControl}. Rating change: ${ratingChange}.`,
+        `Two titans clashed... and neither won. **${username}** drew a ${timeControl} game. Rating change: ${ratingChange}.`,
+        `Not a win, not a loss, just a whole lot of wasted time. **${username}** drew in ${timeControl}.`,
+        `Some say a draw is just a loss without consequences. **${username}** couldn't decide today. Rating change: ${ratingChange}.`,
+        `50 moves, 3 blunders, 2 missed mates, and a stalemate. **${username}** secures the most unsatisfying result in chess: a draw. Rating change: ${ratingChange}.`,
+        `It's not about winning or losing, it's about sending a message. **${username}** just drew a ${timeControl} game.`,
+        `You either die a hero or live long enough to see yourself draw. **${username}** just got ${ratingChange} points with a ${timeControl} draw.`,
+        `Another draw? **${username}** might be allergic to winning. ${timeControl} game ends in a tie.`,
+        `Somewhere, a chess coach just sighed. **${username}** drew in ${timeControl}.`,
+        `A draw? Really? **${username}** out here making sure nobody has fun.`,
         `Congratulations to **${username}**, who managed to snatch a stalemate from the jaws of defeat with a rating of ${newRating} from a ${timeControl} game.!`,
         `Well, **${username}** didn’t win, but at least they didn’t lose either. Current rating: ${newRating}.`,
         `At least you didn’t lose your king, **${username}**. The monarchy goes on, with a rating of ${newRating}.`,
         `Tal once said "To play for a draw, at any rate with white, is to some degree a crime against chess."\nSo someone needs to arrest **${username}**`
     ];
 
-    return format_message(randomDrawMessages[Math.floor(Math.random() * randomDrawMessages.length)], gameUrl, username, 0xFFFFFF);
+    return format_message(randomDrawMessages[Math.floor(Math.random() * randomDrawMessages.length)], game, username, 0xFFFFFF, newRating);
 }
 
-function format_message(message, game_url, username, color = 0x630031) {
+function game_info(game, username, newRating) {
+
+    //const accuracy = username === game.white.username ? game.accuracies.white : game.accuracies.black;
+    return `
+        > - Opening: ${getOpeningName(game.eco)}
+        > - Number of moves: ${getGameLengthFromPGN(game.pgn)}
+        > - New Rating: ${newRating}
+        ### Click below to see analysis of the game!`;
+}
+
+function format_message(message, game, username, color = 0x630031, newRating) {
+    console.log(`Format Message: ${message}`);
     const profile_url = `https://www.chess.com/member/${username.toLowerCase()}`;
     return JSON.stringify({
         embeds: [
             {
                 title: message,
-                description: "Check out the game here!",
+                description: game_info(game, username, newRating),
                 color: color
             }
         ],
         components: [
             {
-                type: 1, // An action row
+                type: 1, // An action for the buttons
                 components: [
                     {
                         type: 2,       // A button
-                        label: "View Game",
+                        label: "Game Link",
                         style: 5,      // 5 is the 'Link' style
-                        url: game_url   // The actual link
+                        url: game.url   // The actual link
                     },
                     {
                         type: 2,
-                        label: `View ${username}'s profile`,
+                        label: `View ${username}'s Profile`,
                         style: 5,
                         url: profile_url
+                    },
+                    {
+                        type: 2,
+                        label: `See Opening`,
+                        style: 5,
+                        url: game.eco
                     }
                 ]
             }
